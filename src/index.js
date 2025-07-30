541af25b8ed1ffd11e4f5956b994dff3d3431c53
Pedro Paulo Brauna Calazans <pedropaulo91.calazans@gmail.com>
2025-07-29 23:19:44 -0300

    feat: Implementação do  menu inicial + Add lib chalk.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import prompt from "prompt";
 
-async function main(params) {}
+import mainPrompt from "./prompts/prompt-main.js";
+import chalk from "chalk";
+
+async function main(params) {
+  prompt.get(mainPrompt, async (err, result) => {
+    if (result.select == 1) console.log(chalk.green("Escolheu o QRCode"));
+    if (result.select == 2) console.log(chalk.green("Escolheu o Password"));
+  });
+
+  prompt.start();
+}
 
 main();
