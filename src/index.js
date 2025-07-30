1d53eb09895df7902b83934051af7d7e65b6db38
Pedro Paulo Brauna Calazans <pedropaulo91.calazans@gmail.com>
2025-07-29 23:56:33 -0300

    feat: Add lib qrcode + organização da pastar para criação do qrcode.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import prompt from "prompt";
+import chalk from "chalk";
 
 import mainPrompt from "./prompts/prompt-main.js";
-import chalk from "chalk";
+import createQRCode from "./services/qr-code/create.js";
 
 async function main(params) {
   prompt.get(mainPrompt, async (err, result) => {
-    if (result.select == 1) console.log(chalk.green("Escolheu o QRCode"));
+    if (result.select == 1) await createQRCode();
     if (result.select == 2) console.log(chalk.green("Escolheu o Password"));
   });
 
---
+++ src/services/qr-code/create.js
@@ -0,0 +1,7 @@
+import prompt from "prompt";
+
+import promptQRCode from "../../prompts/prompt-qrcode";
+
+async function createQRCode() {}
+
+export default createQRCode;
