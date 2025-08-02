509cb28acc3ba3c57aefe3df1d3741f86dd7c11b
Pedro Paulo Brauna Calazans <pedropaulo91.calazans@gmail.com>
2025-08-01 22:10:00 -0300

    feat: Add implementação do create password.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import chalk from "chalk";
 
 import mainPrompt from "./prompts/prompt-main.js";
 import createQRCode from "./services/qr-code/create.js";
+import createPassword from "./services/password/create.js";
 
 async function main(params) {
   prompt.get(mainPrompt, async (err, result) => {
     if (result.select == 1) await createQRCode();
-    if (result.select == 2) console.log(chalk.green("Escolheu o Password"));
+    if (result.select == 2) await createPassword();
   });
 
   prompt.start();
